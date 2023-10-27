02a6a82fd6e21fb029be434b9b369f4ca7cffc2b
Madihakhan1 <144325902+Madihakhan1@users.noreply.github.com>
2023-10-27 12:13:33 +0200

    Add Conflict

---
+++ gitHubDemo/src/main/java/org/example/Conflict.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Conflict {
+
+
+    Conflict() {
+
+    }
+    public String mergeConflict() {
+        String name = "Madiha";
+        return name;
+
+    }
+}
--- gitHubDemo/src/main/java/org/example/Main.java
+++ gitHubDemo/src/main/java/org/example/Main.java
@@ -3,5 +3,8 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Bim bam busse");
+
+        Conflict con = new Conflict();
+        System.out.println(con.mergeConflict());
     }
 }
